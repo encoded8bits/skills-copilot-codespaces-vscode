5d777f196076d2960dd7f467454c02ce82604cb3
tiancodes <csanchezsalas@gmail.com>
2024-01-24 04:31:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,77 +1,23 @@
-// Create a web server
-// Create a web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var comments = require('./comments');
-var mime = require('mime');
+// Create web server
+// Create a web server that listens to incoming requests on port 3000. 
+// When a request is received to the endpoint /comments, it should return an array of comments.
+// The array should have 3 comments (you can come up with the comments yourself).
+// Use the Express web framework to create the server.
 
-var server = http.createServer(function(req, res) {
-  var pathname = url.parse(req.url).pathname;
-  if (pathname === '/') {
-    pathname = '/index.html';
-  }
-  var extname = path.extname(pathname);
-  if (extname === '.html') {
-    res.setHeader('Content-Type', 'text/html');
-  }
-  if (extname === '.css') {
-    res.setHeader('Content-Type', 'text/css');
-  }
-  if (extname === '.js') {
-    res.setHeader('Content-Type', 'text/javascript');
-  }
-  if (extname === '.jpg') {
-    res.setHeader('Content-Type', 'image/jpeg');
-  }
-  if (extname === '.png') {
-    res.setHeader('Content-Type', 'image/png');
-  }
-  if (extname === '.gif') {
-    res.setHeader('Content-Type', 'image/gif');
-  }
-  if (extname === '.json') {
-    res.setHeader('Content-Type', 'application/json');
-  }
-  fs.readFile(path.join(__dirname, pathname), function(err, data) {
-    if (err) {
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
-      res.end('Not Found');
-    } else {
-      res.writeHead(200, {
-        'Content-Type': mime.lookup(extname)
-      });
-      res.end(data);
-    }
-  });
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const comments = [
+    { id: 1, author: "John Doe", body: "Hello, world!" },
+    { id: 2, author: "Jane Doe", body: "Hello, world!" },
+    { id: 3, author: "Jim Doe", body: "Hello, world!" }
+];
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
 
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function(socket) {
-  socket.on('getAllComments', function() {
-    comments.getAll(function(err, comments) {
-      if (err) {
-        socket.emit('error', err);
-      } else {
-        socket.emit('allComments', comments);
-      }
-    });
-  });
-  socket.on('addComment', function(comment) {
-    comments.add(comment, function(err) {
-      if (err) {
-        socket.emit('error', err);
-      } else {
-        socket.emit('commentAdded');
-        socket.broadcast.emit('commentAdded', comment);
-      }
-    });
-  });
-});
-
-server.listen(3000, function() {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
