80879de6fc2503281c059c0d5657e5a8477ab02e
tiancodes <csanchezsalas@gmail.com>
2024-01-24 04:20:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,31 +1,77 @@
 // Create a web server
-// Run: node comments.js
-// Test: curl -d "comment=Hello" http://localhost:3000/comments
+// Create a web server
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var comments = require('./comments');
+var mime = require('mime');
 
-const http = require('http');
-const url = require('url');
-const qs = require('querystring');
-
-const comments = [];
-
-http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    let body = '';
-    req.setEncoding('utf-8');
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
-    req.on('end', () => {
-      const params = qs.parse(body);
-      comments.push(params.comment);
-      res.end('ok\n');
-    });
-  } else if (req.method === 'GET') {
-    res.end(comments.map((comment) => {
-      return comment + '\n';
-    }).join(''));
+var server = http.createServer(function(req, res) {
+  var pathname = url.parse(req.url).pathname;
+  if (pathname === '/') {
+    pathname = '/index.html';
   }
-}).listen(3000);
-```
+  var extname = path.extname(pathname);
+  if (extname === '.html') {
+    res.setHeader('Content-Type', 'text/html');
+  }
+  if (extname === '.css') {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  if (extname === '.js') {
+    res.setHeader('Content-Type', 'text/javascript');
+  }
+  if (extname === '.jpg') {
+    res.setHeader('Content-Type', 'image/jpeg');
+  }
+  if (extname === '.png') {
+    res.setHeader('Content-Type', 'image/png');
+  }
+  if (extname === '.gif') {
+    res.setHeader('Content-Type', 'image/gif');
+  }
+  if (extname === '.json') {
+    res.setHeader('Content-Type', 'application/json');
+  }
+  fs.readFile(path.join(__dirname, pathname), function(err, data) {
+    if (err) {
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Not Found');
+    } else {
+      res.writeHead(200, {
+        'Content-Type': mime.lookup(extname)
+      });
+      res.end(data);
+    }
+  });
+});
 
-###
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket) {
+  socket.on('getAllComments', function() {
+    comments.getAll(function(err, comments) {
+      if (err) {
+        socket.emit('error', err);
+      } else {
+        socket.emit('allComments', comments);
+      }
+    });
+  });
+  socket.on('addComment', function(comment) {
+    comments.add(comment, function(err) {
+      if (err) {
+        socket.emit('error', err);
+      } else {
+        socket.emit('commentAdded');
+        socket.broadcast.emit('commentAdded', comment);
+      }
+    });
+  });
+});
+
+server.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
